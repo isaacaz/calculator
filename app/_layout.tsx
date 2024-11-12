269a296484df4eb79db3c124375ac47b093f89d9
isaacaz <oiazisaac@gmail.com>
2024-11-12 09:42:01 -0400

    Firt commit

---
+++ app/_layout.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import { Slot } from "expo-router";
+import React from "react";
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text>RootLayout</Text>
+      <Slot />
+    </View>
+  );
+};
+
+export default RootLayout;
